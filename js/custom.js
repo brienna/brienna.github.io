a594d51e15b056d5e83ddcaecc9d36348940ef8f
Brienna Herold <briennakh@gmail.com>
2021-02-23 16:42:56 -0500

    Set up prelim form

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function() {
 
+	/* Controls filtering animation */
 	$('.projects').isotope({
 		itemSelector: '.project',
 		layoutMode: 'fitRows'
@@ -19,6 +20,66 @@ $(document).ready(function() {
 	});
 
 
+	$("form").submit(function(e){
+		// Return false, so that the form submits through jQuery
+		// and does not reload the page
+		if (e.preventDefault) e.preventDefault();
+		else e.returnValue = false;
+
+		// Set some variables
+		error = 0;
+
+		// Validate all fields (they can't be empty)
+		$(this).find('.validate-required').each(function(){
+			if ($(this).val() === ''){
+				$(this).addClass('field-error');
+				error = 1;
+			} else{
+				$(this).removeClass('field-error');
+			}
+		});
+
+		// Validate email
+		$(this).find('.validate-email').each(function(){
+			if(!(/(.+)@(.+){2,}\.(.+){2,}/.test($(this).val()))){
+				$(this).addClass('field-error');
+				error = 1;
+			}else{
+				$(this).removeClass('field-error');
+			}
+		});
+
+		// If validation is OK, send form, otherwise show error
+		if (error == 1) {
+			// Show error dialog
+			$('.form-error').fadeIn(200);
+			setTimeout(function(){$('.form-error').fadeOut(500); }, 5000);
+		} else {
+			// Hide error dialog
+			$('.form-error').fadeOut(200);
+
+		  	$.ajax({
+			    type: "POST",
+			    url: $(this).attr("action"),
+			    crossDomain: true,
+			    data: new FormData(this),
+			    dataType: "json",
+			    contentType: "multipart/form-data",
+			    processData: false,
+			    contentType: false,
+			    headers: {
+			      "Accept": "application/json"
+			    }
+		  	}).done(function(e) {
+		  		//console.log(e);
+		  		console.log("sent...");
+		     	$('.success').fadeIn(1000);
+				setTimeout(function(){$('.success').fadeOut(500); }, 5000);
+		  	}).fail(function() {
+		     	alert('An error occurred please try again later.');
+		  	});
+		}
+	});
 
 
 
